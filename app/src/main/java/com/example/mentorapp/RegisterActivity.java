b9ca2de10353eb42ff8bb525fc0966a119a47c60
Ghost <avs.adh96@gmail.com>
2019-02-22 20:41:30 +0545

    First commit

---
+++ app/src/main/java/com/example/mentorapp/RegisterActivity.java
@@ -0,0 +1,69 @@
+package com.example.mentorapp;
+
+import android.content.Intent;
+import android.support.annotation.NonNull;
+import android.support.design.widget.TextInputEditText;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+
+public class RegisterActivity extends AppCompatActivity {
+
+    private TextInputEditText mDisplayName;
+    private TextInputEditText mEmail;
+    private TextInputEditText mPassword;
+    private Button mCreateButton;
+
+    private FirebaseAuth mAuth;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+
+        mAuth = FirebaseAuth.getInstance();
+        mDisplayName = findViewById(R.id.reg_name);
+        mEmail = findViewById(R.id.reg_email);
+        mPassword = findViewById(R.id.reg_password);
+        mCreateButton = findViewById(R.id.reg_create_btn);
+
+        mCreateButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String display_name = mDisplayName.getEditableText().toString().trim();
+                String email = mEmail.getEditableText().toString().trim();
+                String password = mPassword.getEditableText().toString().trim();
+
+                register_user(email, password);
+            }
+        });
+    }
+
+    private void register_user(String email, String password) {
+
+        mAuth.createUserWithEmailAndPassword(email, password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                if (task.isSuccessful()) {
+
+                    Intent mainIntent = new Intent(RegisterActivity.this, MainActivity.class);
+                    startActivity(mainIntent);
+                    finish();
+
+                } else {
+                    Toast.makeText(RegisterActivity.this, "You got some Error", Toast.LENGTH_LONG).show();
+                }
+            }
+        });
+
+
+    }
+}
---
+++ app/src/main/java/com/example/mentorapp/StartActivity.java
@@ -0,0 +1,28 @@
+package com.example.mentorapp;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class StartActivity extends AppCompatActivity {
+
+    private Button mRegButton;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_start);
+
+        mRegButton = findViewById(R.id.start_reg_btn);
+        mRegButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(StartActivity.this, RegisterActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+
+}
